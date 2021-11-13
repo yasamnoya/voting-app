ba8be2c0e58127f8ac98a329e1214312b72427f6
yasamnoya <yasamnono@gmail.com>
2021-11-13 23:33:41 +0800

    feature: create new poll from frontend

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -14,6 +14,7 @@ const auth = async (req, res, next) => {
 
     next();
   } catch (e) {
+    console.log(e);
     res.redirect("/login");
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ router.get('/', async (req, res) => {
   res.render('home', { pollData });
 });
 
-router.get('/new-poll', auth, (req, res) => {
+router.get('/new-poll', (req, res) => {
   res.render('new-poll');
 });
 
